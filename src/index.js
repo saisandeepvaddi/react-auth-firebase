d93bdda308ab0caf73d8cef3a671467f423a4af8
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2018-03-10 02:03:24 -0600

    Add to-do comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log(`First Line`);
+import React, { Component } from "react";
+
+export const withFirebaseAuth = (config = {}) => WrappedComponent => {
+  // Validate config
+  // Check for /users at firebase
+  // Add firebase methods
+
+  // Return HOC
+  return class extends Component {
+    render() {
+      return <WrappedComponent {...props} />;
+    }
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist")
+  },
   module: {
     rules: [{ test: /\.(js)$/, use: "babel-loader" }]
   }
