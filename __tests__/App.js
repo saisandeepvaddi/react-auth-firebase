999fedfaf777e93deeb16bf4b31683f41ed07133
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2018-03-10 22:48:15 -0600

    Sign In start

--- __tests__/App.js
+++ __tests__/App.js
@@ -2,17 +2,23 @@ import React, { Component } from "react";
 import withFirebaseAuth from "../dist";
 import firebase, { config as firebaseConfig } from "./firebase";
 
+const email = "test@test.com";
+const password = "password";
+
 class App extends Component {
   render() {
     console.log(this.props);
 
+    const { signIn, signOut, signUp, user } = this.props;
+    signIn(email, password);
     return (
       <div>
         <h1>Testing</h1>
+        {user ? <pre>{JSON.stringify(user, null, 2)}</pre> : null}
       </div>
     );
   }
 }
 
 // export default App;
-export default withFirebaseAuth(App, firebaseConfig);
+export default withFirebaseAuth(App, firebase);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,79 @@
 import React, { Component } from "react";
+import firebase from "firebase";
 
-const withFirebaseAuth = (WrappedComponent, config) => {
+const withFirebaseAuth = (WrappedComponent, firebase) => {
   // Validate config
   // Check for /users at firebase
   // Add firebase methods
 
   // Return HOC
-  if (!config) {
-    console.error("Firebase config file is not passed to withFirebaseAuth()");
-    console.warn("usage: withFirebaseAuth(Component, firebaseConfig)");
-  }
-  if (JSON.stringify(config) === JSON.stringify({})) {
-    console.error("Empty firebase config file");
-  }
-
-  console.log(config);
+  // if (!config) {
+  //   console.error("Firebase config file is not passed to withFirebaseAuth()");
+  //   console.warn("usage: withFirebaseAuth(Component, firebaseConfig)");
+  // }
+  // if (JSON.stringify(config) === JSON.stringify({})) {
+  //   console.error("Empty firebase config file");
+  // }
 
   return class extends Component {
+    constructor(props) {
+      super(props);
+      this.signIn = this.signIn.bind(this);
+      this.signOut = this.signOut.bind(this);
+      this.signUp = this.signUp.bind(this);
+      this.state = {
+        user: null
+      };
+    }
+
+    componentDidMount() {
+      firebase.auth().onAuthStateChanged(
+        function(user) {
+          if (user) {
+            this.setState(() => ({
+              user
+            }));
+            // ...
+          } else {
+            // User is signed out.
+            // ...
+            this.setState(() => ({
+              user: null
+            }));
+          }
+        }.bind(this)
+      );
+    }
+
+    signIn(email, password) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+        });
+      console.log("Sign In Called");
+    }
+    signOut() {
+      console.log("Sign Out Called");
+    }
+    signUp() {
+      console.log("Sign Up Called");
+    }
+
     render() {
-      return <WrappedComponent {...this.props} />;
+      return (
+        <WrappedComponent
+          signIn={this.signIn}
+          signOut={this.signOut}
+          signUp={this.signUp}
+          user={this.state.user}
+          {...this.props}
+        />
+      );
     }
   };
 };
